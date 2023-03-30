d9dc1b2a58768e88de8f587d100b9c570a4a1ada
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-03-30 17:16:19 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/qrgeneratorexample/QrGeneratorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrgeneratorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrGeneratorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrGeneratorExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qrgeneratorexample/QrGeneratorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrgeneratorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrGeneratorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
