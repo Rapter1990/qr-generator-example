56f3038e72fa97d65d870a287cf1f591f4672210
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-04-01 20:25:45 +0300

    Task 11 : Define QrServiceTest and Add sample image

---
+++ src/test/java/com/example/qrgeneratorexample/service/QrServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.qrgeneratorexample.service;
+
+import com.example.qrgeneratorexample.base.BaseServiceTest;
+import com.example.qrgeneratorexample.data.DummyData;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class QrServiceTest extends BaseServiceTest {
+
+    @InjectMocks
+    QrService qrService;
+
+    @SneakyThrows
+    @Test
+    void createQrCodeWithoutImage() {
+        var requestDTO = DummyData.getDummyCreateQrRequestDTO();
+
+        var responseDTO = qrService.createQrCode(requestDTO);
+
+
+        assertThat(responseDTO).isNotNull();
+        assertThat(responseDTO.getBody()).isNotNull().isNotEmpty();
+
+        var binaryBitMap = new BinaryBitmap(new HybridBinarizer(
+                new BufferedImageLuminanceSource(ImageIO.read(new ByteArrayInputStream(responseDTO.getBody())))));
+
+        var result = new MultiFormatReader().decode(binaryBitMap);
+
+        assertEquals(result.getText(), requestDTO.getText());
+        assertEquals(result.getBarcodeFormat(), BarcodeFormat.QR_CODE);
+
+    }
+
+    @SneakyThrows
+    @Test
+    void createQrCodeWithImage() {
+        var requestDTO = DummyData.getDummyCreateQrRequestDTO();
+        requestDTO.setImage(new ClassPathResource("java-original-wordmark.png").getInputStream().readAllBytes());
+
+        var responseDTO = qrService.createQrCode(requestDTO);
+
+        assertThat(responseDTO).isNotNull();
+        assertThat(responseDTO.getBody()).isNotNull().isNotEmpty();
+
+
+        var binaryBitMap = new BinaryBitmap(new HybridBinarizer(
+                new BufferedImageLuminanceSource(ImageIO.read(new ByteArrayInputStream(responseDTO.getBody())))));
+
+        var result = new MultiFormatReader().decode(binaryBitMap);
+
+        assertEquals(result.getText(), requestDTO.getText());
+        assertEquals(result.getBarcodeFormat(), BarcodeFormat.QR_CODE);
+
+    }
+}
